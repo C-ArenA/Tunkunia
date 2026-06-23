f3a5947ea28c20afdcc74062b2bc12cd91a3e2d4
C-ArenA <carlostata.ca@gmail.com>
2026-06-23 02:37:06 -0400

    feat: start displaying trámites

---
+++ spa/app/pages/hola.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+interface Tramite {
+  id: number
+  name: string
+  description: string
+}
+interface CollectionResponse<T> {
+  data: T[]
+  next_page_url: string
+  previous_page_url: string
+}
+
+const { data } = await useFetch<CollectionResponse<Tramite>>('/api/v1/catalog/tramites', {
+  method: 'GET',
+  baseURL: 'http://localhost:8080',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+const title = 'Trámites Disponibles'
+
+</script>
+
+<template>
+  <div class="px-12 pt-8">
+    <h1 class="text-3xl pb-4 font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+      {{ title }}
+    </h1>
+    <UTable :data="data?.data" class="flex-1" />
+  </div>
+</template>
+
+<style scoped></style>
