336a12997628434cf7739a5404c6fb41f35339a5
C-ArenA <carlostata.ca@gmail.com>
2026-07-13 11:30:47 -0400

    feat: simplify key generation comand

--- cmd/api_key_generate.go
+++ cmd/api_key_generate.go
@@ -8,8 +8,8 @@ import (
 	"io"
 	"log"
 	"os"
-	"strings"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
 )
 
@@ -24,8 +24,7 @@ func NewApiKeyGenerateCmd() *cobra.Command {
 func createSecretKey() string {
 	secretBytes := make([]byte, 32)
 	if _, err := io.ReadFull(rand.Reader, secretBytes); err != nil {
-		fmt.Printf("❌ Error generating random bytes: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("❌ Error generando bytes aleatorios: %v", err)
 	}
 	return hex.EncodeToString(secretBytes)
 }
@@ -33,32 +32,16 @@ func createSecretKey() string {
 func apiKeyGenerate(cmd *cobra.Command, args []string) {
 	envFile := ".env"
 	keyName := "JWT_SECRET"
-	newLine := fmt.Sprintf("%s=\"%s\"", keyName, createSecretKey())
 
-	originalEnvData, err := os.ReadFile(envFile)
+	env, err := godotenv.Read(envFile)
 	if err != nil && !errors.Is(err, os.ErrNotExist) {
-		log.Fatalf("❌ Error reading existing .env file: %v", err)
+		log.Fatalf("Error al leer archivo existente %s: %v", envFile, err)
 	}
 
-	var content []string
-	updated := false
-	if len(originalEnvData) > 0 {
-		content = strings.Split(string(originalEnvData), "\n")
-		for i := range content {
-			trimmed := strings.TrimSpace(content[i])
-			if strings.HasPrefix(trimmed, keyName+"=") {
-				content[i] = newLine
-				updated = true
-			}
-		}
-	}
-	if !updated {
-		content = append(content, newLine)
-	}
+	env[keyName] = createSecretKey()
 
-	if err := os.WriteFile(envFile, []byte(strings.Join(content, "\n")), 0644); err != nil {
-		log.Fatalf("❌ Error writing to %s file: %v\n", envFile, err)
+	if err := godotenv.Write(env, envFile); err != nil {
+		log.Fatalf("Error al guardar archivo %s: %v", envFile, err)
 	}
-
-	fmt.Println("✅ Successfully generated HS256-compliant key!")
+	fmt.Println("✅ Llave HS256 generada y guardada!")
 }
--- config/config.go
+++ config/config.go
@@ -14,7 +14,7 @@ type Config struct {
 	Env           string `env:"ENV"`
 	GooseDriver   string `env:"GOOSE_DRIVER"`
 	GooseDbString string `env:"GOOSE_DBSTRING"`
-	JWTKey        string `env:"JWT_KEY"`
+	JWTSecret     string `env:"JWT_SECRET"`
 	AppName       string
 }
 
