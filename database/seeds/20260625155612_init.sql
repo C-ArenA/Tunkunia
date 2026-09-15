86ff814fa8d65d0829f1283c835d450347e165e3
C-ArenA <carlostata.ca@gmail.com>
2026-09-15 01:45:43 -0400

    feat: add demo users

--- database/seeds/20260625155612_init.sql
+++ database/seeds/20260625155612_init.sql
@@ -10,5 +10,45 @@ VALUES (
         'Este es otro trámite de prueba para fines de demostración.',
         '1. Revisar requisitos\n2. Llenar formulario\n3. Enviar solicitud'
     ) ON CONFLICT (name) DO NOTHING;
+INSERT INTO users (
+        name,
+        sub,
+        email,
+        email_verified,
+        is_admin,
+        is_public_servant
+    )
+VALUES (
+        'Carlos Arena',
+        '021196',
+        'carlostata.ca@gmail.com',
+        1,
+        1,
+        1
+    ),
+    (
+        'Roberto Zambrana',
+        '010119191',
+        'roberto.zambrana@gmail.com',
+        1,
+        1,
+        1
+    ),
+    (
+        'Fabián Tito',
+        '010119192',
+        'fabiantito@gmail.com',
+        1,
+        1,
+        1
+    ),
+    (
+        'Jorge Nava',
+        '0101191933',
+        'janava@gmail.com',
+        1,
+        1,
+        1
+    ) ON CONFLICT (sub) DO NOTHING;
 -- +goose Down
 SELECT 'down SQL query';
