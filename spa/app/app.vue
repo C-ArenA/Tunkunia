000d106b8e93c2bb8a13f7ae8a6c1c028459b59f
C-ArenA <carlostata.ca@gmail.com>
2026-09-16 15:41:05 -0400

    feat: add demo badge

--- spa/app/app.vue
+++ spa/app/app.vue
@@ -1,8 +1,11 @@
+<script setup lang="ts"></script>
+
 <template>
   <UApp>
     <NuxtRouteAnnouncer />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <UiDemoBadge />
   </UApp>
 </template>
---
+++ spa/app/components/ui/DemoBadge.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useQuery } from "@pinia/colada";
+import { getInstitutionQuery } from "#shared/clientV1/@pinia/colada.gen";
+const { data: institution, } = useQuery(getInstitutionQuery);
+</script>
+
+<template>
+  <div
+    v-if="institution?.demo"
+    class="fixed bottom-0 right-4 z-50 rounded-t-md bg-slate-950 px-1 py-1 text-xs font-semibold text-white shadow-2xl opacity-60 pointer-events-none"
+  >
+    DEMO
+  </div>
+</template>
+
+<style scoped></style>
